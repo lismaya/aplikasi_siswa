2bdcde025ca2ad1e338c0336c21274ed4f193cb8
lismaya <lismayaok@gmail.com>
2020-06-22 22:11:56 +0700

    template klorofil

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,42 +1,47 @@
 @extends('layouts.master')
 @section('content')
-      @if(session('sukses'))
-      <div class="alert alert-success" role="alert">
-        {{session('sukses')}}
-      </div>
-      @endif
-      <h1>Edit Data Siswa</h1>
-      <div class="row">
-        <div class="col-lg-12">
-        <form action="/siswa/{{$siswa->id}}/update" method="post">
-          {{csrf_field()}}
-        <div class="form-group">
-          <label for="exampleInputEmail1">Nama Depan</label>
-          <input type="text" name="nama_depan" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Nama Depan" value="{{$siswa->nama_depan}}">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Nama Belakang</label>
-          <input type="text" name="nama_belakang" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Nama Belakang" value="{{$siswa->nama_belakang}}">
-        </div>
-        <div class="form-group">
-         <label for="exampleFormControlSelect1">Jenis Kelamin</label>
-         <select class="form-control" name="jenis_kelamin" id="exampleFormControlSelect1">
-           <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-Laki</option>
-           <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
-         </select>
-       </div>
-       <div class="form-group">
-         <label for="exampleInputEmail1">Agama</label>
-         <input type="text" name="agama" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Agama" value="{{$siswa->agama}}">
-       </div>
-       <div class="form-group">
-        <label for="exampleFormControlTextarea1">Alamat</label>
-        <textarea class="form-control" name="alamat" id="exampleFormControlTextarea1" rows="3">{{$siswa->alamat}}</textarea>
-      </div>
-        <button type="submit" class="btn btn-warning">Update</button>
-      </form>
-        </div>
-
-      </div>
-
+<div class="main">
+  <div class="main-content">
+    <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Input Sizing</h3>
+								</div>
+								<div class="panel-body">
+                  <form action="/siswa/{{$siswa->id}}/update" method="post">
+                    {{csrf_field()}}
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Depan</label>
+                    <input type="text" name="nama_depan" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Nama Depan" value="{{$siswa->nama_depan}}">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Belakang</label>
+                    <input type="text" name="nama_belakang" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Nama Belakang" value="{{$siswa->nama_belakang}}">
+                  </div>
+                  <div class="form-group">
+                   <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                   <select class="form-control" name="jenis_kelamin" id="exampleFormControlSelect1">
+                     <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-Laki</option>
+                     <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                   </select>
+                 </div>
+                 <div class="form-group">
+                   <label for="exampleInputEmail1">Agama</label>
+                   <input type="text" name="agama" class="form-control" id="exampleInputEmail1" aria-describedby="Masukkan Agama" value="{{$siswa->agama}}">
+                 </div>
+                 <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Alamat</label>
+                  <textarea class="form-control" name="alamat" id="exampleFormControlTextarea1" rows="3">{{$siswa->alamat}}</textarea>
+                </div>
+                  <button type="submit" class="btn btn-warning">Update</button>
+                </form>
+								</div>
+							</div>
+            </div>
+          </div>
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,50 +1,59 @@
 @extends('layouts.master')
 @section('content')
-      @if(session('sukses'))
-      <div class="alert alert-success" role="alert">
-        {{session('sukses')}}
-      </div>
-      @endif
-      <div class="row">
-        <div class="col-6">
-          <h1>DATA SISWA</h1>
-        </div>
-        <div class="col-6">
-          <!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
-            Tambah Data Siswa
-          </button>
+<div class="main">
+  <div class="main-content">
+    <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="panel">
+                @if(session('sukses'))
+                <div class="alert alert-success" role="alert">
+                  {{session('sukses')}}
+                </div>
+                @endif
+                <div class="panel-heading">
+                  <h1 class="panel-title">Data Siswa</h1>
+                  <div class="right">
+                    <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal" ><i class="lnr lnr-plus-circle"> </i>
+                    </button>
+                  </div>
+                </div>
+                <div class="panel-body">
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>NAMA DEPAN</th>
+                        <th>NAMA BELAKANG</th>
+                        <th>JENIS KELAMIN</th>
+                        <th>AGAMA</th>
+                        <th>ALAMAT</th>
+                        <th>AKSI</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach( $data_siswa as $siswa)
+                      <tr>
+                          <td>{{$siswa->nama_depan}}</td>
+                          <td>{{$siswa->nama_belakang}}</td>
+                          <td>{{$siswa->jenis_kelamin}}</td>
+                          <td>{{$siswa->agama}}</td>
+                          <td>{{$siswa->alamat}}</td>
+                          <td><a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm"> Edit </a>
+                            <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin mau dihapus?')"> Delete </a>
+                          </td>
 
-
-        </div>
-
-
-        <table class="table table-hover">
-          <tr>
-            <th>NAMA DEPAN</th>
-            <th>NAMA BELAKANG</th>
-            <th>JENIS KELAMIN</th>
-            <th>AGAMA</th>
-            <th>ALAMAT</th>
-            <th>AKSI</th>
-          </tr>
-          @foreach( $data_siswa as $siswa)
-          <tr>
-              <td>{{$siswa->nama_depan}}</td>
-              <td>{{$siswa->nama_belakang}}</td>
-              <td>{{$siswa->jenis_kelamin}}</td>
-              <td>{{$siswa->agama}}</td>
-              <td>{{$siswa->alamat}}</td>
-              <td><a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm"> Edit </a>
-                <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin mau dihapus?')"> Delete </a>
-              </td>
-
-            </tr>
-          @endforeach
-        </table>
-      </div>
-
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
+  </div>
+</div>
+
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
