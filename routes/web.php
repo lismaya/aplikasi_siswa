b2e7a7b16e37eb11ddc350a8306faf21f63eabb4
lismaya <lismayaok@gmail.com>
2020-06-23 11:23:06 +0700

    login dan middleware

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/siswa','SiswaController@index');
-Route::post('siswa/create','SiswaController@create');
-Route::get('/siswa/{id}/edit','SiswaController@edit');
-Route::post('/siswa/{id}/update','SiswaController@update');
-Route::get('/siswa/{id}/delete','SiswaController@delete');
+Route::get('/login','AuthController@login')->name('login');
+Route::post('/postLogin','AuthController@postLogin');
+Route::get('/logout','AuthController@logout');
+
+
+Route::group(['middleware'=>'auth'],function(){
+  Route::get('/dashboard','DashboardController@index')->middleware('auth');
+  Route::get('/siswa','SiswaController@index');
+  Route::post('siswa/create','SiswaController@create');
+  Route::get('/siswa/{id}/edit','SiswaController@edit');
+  Route::post('/siswa/{id}/update','SiswaController@update');
+  Route::get('/siswa/{id}/delete','SiswaController@delete');
+});
